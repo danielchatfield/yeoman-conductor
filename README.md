e8dc6cb0754ff357e2803259f397c7d3b7cca510
Daniel Chatfield <chatfielddaniel@gmail.com>
2013-08-12 19:01:27 +0100

    Working yeoman-conductor

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-yeoman-conductor
-================
+# yeoman-conductor [![Build Status](https://secure.travis-ci.org/danielchatfield/yeoman-conductor.png?branch=master)](http://travis-ci.org/danielchatfield/yeoman-conductor)
 
-A node module that let's you run a yeoman generator from another one.
+A module that let's you run a yeoman generator from another one.
+
+## Getting Started
+Install the module with: `npm install yeoman-conductor`
+
+```javascript
+var conductor = require('yeoman-conductor');
+var oldGenerator = require('generator-something');
+
+conductor.extend(newGenerator, oldGenerator);
+```
+
+## Documentation
+
+### conductor.extend(newGenerator, oldGenerator)
+
+## License
+Copyright (c) 2013 Daniel Chatfield. Licensed under the MIT license.
---
+++ test/yeoman-conductor_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var conductor = require('../lib/yeoman-conductor.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['yeoman-conductor'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'run is func': function(test) {
+    test.expect(1);
+    // tests here
+    test.ok(typeof conductor.run === 'function', 'conductor.run should be a function.');
+    test.done();
+  }
+};
